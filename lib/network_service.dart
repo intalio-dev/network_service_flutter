69343721f3ef2ab8baf9c40d3db5a9b889da7985
Ahmed Shawky <shawkyahmed392@gmail.com>
2025-10-08 13:12:02 +0300

    Update network_service.dart

--- lib/network_service.dart
+++ lib/network_service.dart
@@ -7,4 +7,5 @@ export 'src/service/default_headers_interceptor.dart';
 export 'src/models/network_base_model.dart';
 export 'src/models/network_connectivity_model.dart';
 export 'src/models/network_exceptions.dart';
+export 'src/models/network_error_details.dart';
 export 'src/models/network_result.dart';
